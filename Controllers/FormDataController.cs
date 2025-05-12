acafe416a9ec1776737a9c7b57cb26a3d1c97232
Shenoy <saakshi.shenoy@saks.tech>
2025-05-12 22:18:42 +0530

    Week 3 Day 5 Added

---
+++ Controllers/FormDataController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Week3_Day5.Controllers
+{
+    public class FormDataController : Controller
+    {
+        string filePath = "data.txt";
+        public IActionResult Index()
+        {
+            List<string> formData = new List<string>();
+
+            if (System.IO.File.Exists(filePath))
+            {
+                formData = System.IO.File.ReadAllLines(filePath).ToList();
+            }
+            else
+            {
+                formData = new List<string>();
+            }
+            return View(formData);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string emailId, string message)
+        {
+            if (string.IsNullOrWhiteSpace(emailId)==false && string.IsNullOrWhiteSpace(message)==false)
+            {
+                string newData = $"{emailId}: {message}";
+                System.IO.File.AppendAllText(filePath, newData + "\n");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
